2211bb2a974fae116a635179df4af705392acfac
Miguel Angel Garcia <miguelgarcia@macbook-pro.myfiosgateway.com>
2023-06-13 20:17:03 -0400

    all done
    q
    clear
    
    '

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function randomRGB() {
-    const r = Math.floor(MAth.random() * 256)
-    const g = Math.floor(MAth.random() * 256)
-    const b = Math.floor(MAth.random() * 256)
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
     return `rgb(${r}, ${g},${b})`
 }
 
 const letters = document.querySelectorAll('.letter');
 const intervalid = setInterval(function () {
-    for (let lettor of letters) {
-        letters.style.color = randomRGB();
+    for (let letter of letters) {
+        letter.style.color = randomRGB();
     }
-}, 2000);
+}, 1000);
